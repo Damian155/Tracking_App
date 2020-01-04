3ff26ccf5e317353dde37d2382a6a009881b5eff
DamianNguyen <58244582+DamianNguyen@users.noreply.github.com>
2020-01-04 23:16:19 +0700

    New code include: Openning Layout, Bluetooth Activity, NextActivity

--- app/src/main/java/com/dqt/thanhdu/newauthenticationfirebase/NextActivity.java
+++ app/src/main/java/com/dqt/thanhdu/newauthenticationfirebase/NextActivity.java
@@ -2,13 +2,31 @@ package com.dqt.thanhdu.newauthenticationfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NextActivity extends AppCompatActivity {
-
+    Button blue_buzz_btn,wifi_gps_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+        blue_buzz_btn = (Button) findViewById(R.id.buzzer_btn);
+        wifi_gps_btn = (Button) findViewById(R.id.gps_btn);
+        blue_buzz_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NextActivity.this,DeviceList.class);
+                startActivity(intent);
+            }
+        });
+        wifi_gps_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
